0bc948dd9b5bff64baceead8e1eecbe4f729447a
EnnaLea <49825624+leaaliman@users.noreply.github.com>
2023-01-28 11:42:20 +0100

    change 1

--- dog-class.php
+++ dog-class.php
@@ -6,14 +6,21 @@ class Dog{
     private $dog_color = "no color";
     private $dog_name = "no name";
 
+    function set_dog_name($value){
+        $error_message = TRUE;
+        (ctype_alpha($value)&& strlen($value) < 21) ? $this->dog_name = 
+        $value : $error_message = FALSE;
+        return $error_message; 
+    }
+
     function get_properties()
     {
-        Return "$this->dog_weight, $this->dog_breed, $this->dog_color.";
+        return "$this->dog_weight, $this->dog_breed, $this->dog_color.";
 
     }
 
     function get_speak(){
-        Return "BARK! BARK!";
+        return "BARK! BARK!";
     }
 
 
