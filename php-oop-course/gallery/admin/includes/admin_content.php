a6e20bbe703376ce24651a526bb7315f259549dd
EnnaLea <49825624+leaaliman@users.noreply.github.com>
2023-02-09 11:43:48 +0100

    change2 09/02/2023

--- php-oop-course/gallery/admin/includes/admin_content.php
+++ php-oop-course/gallery/admin/includes/admin_content.php
@@ -1,57 +1,60 @@
-
-
 <div class="container-fluid">
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            ADMIN
-                            <small>Subheading</small>
-                        </h1>
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                ADMIN
+                <small>Subheading</small>
+            </h1>
 
-<?php
+            <?php
 
-// instatiantion of the user object
-$user = new User();
-$user->username = "saph";
-$user->password = "night";
-$user->first_name = "Saphira";
-$user->last_name = "Patata";
-$user->create();
+            // instatiantion of the user object
+            // $user = new User();
+            // $user->username = "asm";
+            // $user->password = "ciccio";
+            // $user->first_name = "Asmar";
+            // $user->last_name = "Cicciotto";
+            // $user->create();
 
-
-
-
-// $user = User::find_user_by_id(4);
-// $user->delete();
-// $user = User::find_user_by_id(6);
-// $user->password = "mozzi";
-// $user->first_name = "Milly";
-// $user->last_name = "Molly";
-// $user->update();
-// $user->save();
-
-// $user->username = "Milly";
-// $user->save();
+            // $users = User::find_all();
+            // foreach ($users as $user) {
+            //     echo $user->username;
+            // }
 
 
 
-?>
+
+            // $user = User::find_user_by_id(4);
+            // $user->delete();
+            // $user = User::find_user_by_id(6);
+            // $user->password = "mozzi";
+            // $user->first_name = "Milly";
+            // $user->last_name = "Molly";
+            // $user->update();
+            // $user->save();
+
+            // $user->username = "Milly";
+            // $user->save();
 
 
 
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-                <!-- /.row -->
+            ?>
 
-            </div>
-            <!-- /.container-fluid -->
+
+
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
+                </li>
+                <li class="active">
+                    <i class="fa fa-file"></i> Blank Page
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+
+</div>
+<!-- /.container-fluid -->
