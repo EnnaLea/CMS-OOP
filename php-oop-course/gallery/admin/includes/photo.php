cb5f48f4026bbdfb42bbb9fac351b4c4e6945b3b
EnnaLea <49825624+leaaliman@users.noreply.github.com>
2023-02-13 11:44:37 +0100

    change

--- php-oop-course/gallery/admin/includes/photo.php
+++ php-oop-course/gallery/admin/includes/photo.php
@@ -47,4 +47,41 @@ class Photo extends Db_object
             $this->size = basename($file['size']);
         }
     }
+
+
+    public function save()
+    {
+        if ($this->photo_id) {
+            $this->update();
+        } else {
+
+            if (!empty($this->errors)) {
+                return false;
+            }
+
+            if (empty($this->filename) || empty($this->tmp_path)) {
+                $this->errors[] = "the file was not available";
+                return false;
+            }
+
+            $target_path = SITE_ROOT . DS . 'amin' . DS . $this->upload_directory . DS . $this->filename;
+
+            if (file_exists($target_path)) {
+                $this->errors[] = "The file{$this->filename} already exists";
+                return false;
+            }
+
+            if (move_uploaded_file($this->tmp_path, $target_path)) {
+                if ($this->create()) {
+                    unset($this->tmp_path);
+                    return true;
+                }
+            } else {
+                $this->errors[] = "The file directory probably doesn't have permission. ";
+                return false;
+            }
+
+            $this->create();
+        }
+    }
 }
